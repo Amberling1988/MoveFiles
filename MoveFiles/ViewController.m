388bbb9e97975982346f8095cce45c7d02204808
Amber <oraling@163.com>
2018-11-13 15:32:22 +0800

    Update MoveFiles/ViewController.m
    增加CGD迭代方法处理剪切

--- MoveFiles/ViewController.m
+++ MoveFiles/ViewController.m
@@ -15,7 +15,7 @@
 @implementation ViewController
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self moveFiles];
+    [self moveFilesWithGCD];
 }
 
 - (void)moveFiles {
@@ -33,6 +33,25 @@
                               
     }
     
+}
+
+- (void)moveFilesWithGCD {
+    
+    NSString *from = @"/Users/linglingmeng/Desktop/iOS书籍";
+    NSString *to = @"/Users/linglingmeng/Desktop/to";
+    
+    NSArray *filesArray = [[NSFileManager defaultManager] subpathsAtPath:from];
+    NSInteger count = filesArray.count;
+    // apply 快速迭代，会开子线程，并发执行
+    // 队列参数一定要是并发队列
+    dispatch_apply(count, dispatch_get_global_queue(0, 0), ^(size_t i) {
+        NSString *fullPath = [from stringByAppendingPathComponent:filesArray[i]];
+        NSString *toFullPath = [to stringByAppendingPathComponent:filesArray[i]];
+        
+        [[NSFileManager defaultManager] moveItemAtPath:fullPath toPath:toFullPath error:nil];
+        
+        NSLog(@"%@ ",[NSThread currentThread]);
+    });
 }
 
 @end
